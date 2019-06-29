7793de667c49c9982187066bd3f936a1ed26de9c
Lenfree Yeung <lenfree.yeung@gmail.com>
2019-06-29 15:50:20 +1000

    tidy up property desc

--- test/ddog_test.exs
+++ test/ddog_test.exs
@@ -5,14 +5,14 @@ defmodule DdogTest do
 
   require IEx
 
-  describe "build_query/1" do
-    property "pass a list of ascii input returns a string with a ' ' in between each element" do
-            check all str <- list_of(string(:ascii)) do
-              assert Helper.build_query(str) == str
-              |> Enum.filter(fn x -> String.length(x) > 0 end)
-              |> List.flatten()
-              |> Enum.join(" ")
-      end
+  describe "pass a list of ascii input returns a string with a ' ' in between each element" do
+    property "list of strings input joins as a string should be equal to build_query/1 output" do
+      check all str <- list_of(string(:ascii)) do
+        assert Helper.build_query(str) == str
+        |> Enum.filter(&String.length(&1) > 0)
+        |> List.flatten()
+        |> Enum.join(" ")
+        end
     end
   end
 end
